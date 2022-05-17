fcb7f090974a11e214185356663c57ca5807717d
CherylL26 <cheryll@tuta.io>
2022-05-16 20:19:03 -0500

    post functionality

--- app.py
+++ app.py
@@ -1,6 +1,13 @@
-from flask import Flask
+from flask import Flask, request
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/", methods = ['POST'])
 def main():
-    return "<p>Test</p>"
+    data = request.get_json()
+    to = data['to']
+    subject = data['subject']
+    body = data['body']
+    return '''
+    to: {}
+    subject: {}
+    body: {}'''.format(to, subject, body)
