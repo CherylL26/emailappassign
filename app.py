1f2fb74523f1456551b6551490c028ac7365e1ba
CherylL26 <cheryll@tuta.io>
2022-05-16 20:57:55 -0500

    flask mail

--- app.py
+++ app.py
@@ -1,5 +1,17 @@
 from flask import Flask, request
+from flask_mail import Mail, Message
 app = Flask(__name__)
+
+mailconf = {
+    "MAIL_SERVER": 'smtp.gmail.com',
+    "MAIL_PORT": 465,
+    "MAIL_USE_TLS": False,
+    "MAIL_USE_SSL": True,
+    "MAIL_USERNAME": 'webdevtesting24@gmail.com',
+    "MAIL_PASSWORD": '$@BFwYu72ucHu!'
+}
+app.config.update(mailconf)
+mail = Mail(app)
 
 @app.route("/", methods = ['POST'])
 def main():
@@ -7,6 +19,10 @@ def main():
     to = data['to']
     subject = data['subject']
     body = data['body']
+    msg = Message('''{}
+    
+    {}'''.format(subject, body), sender=app.config.get("MAIL_USERNAME"), recipients=[to])
+    mail.send(msg)
     return '''
     to: {}
     subject: {}
